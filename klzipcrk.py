f156cbc9733153c49ab8f21943a8a812b299c2d2
juanmadelboca <juanmadelboca@gmail.com>
2017-10-06 01:58:37 -0300

    fixing some issues

--- klzipcrk.py
+++ klzipcrk.py
@@ -1,27 +1,32 @@
 
 import optparse
 import zipfile
-from threading import Thread
-#from concurrent.futures import ThreadPoolExecutor
-import time
 from time import time
+import multiprocessing.dummy as mp
+
 
 def extract_zip(zfile, password):
     try:
         zfile.extractall(pwd=password)
         print "[+] Password Found: " + password + '\n'
+        exit(0)
     except:
         pass
 
+
+def function(string):
+    print string
+
+
 def main():
-    tiempo_inicial = time() 
-    parser = optparse.OptionParser("usage %prog "+"-f <zipfile> -d <dicctionary>")
-    parser.add_option('-f', dest='zname', type='string',help='specify zip file')
-    parser.add_option('-d', dest='dname', type='string',help='specify dictionary file')
+    tiempo_inicial = time()
+    parser = optparse.OptionParser("usage %prog " + "-f <zipfile> -d <dicctionary>")
+    parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
     (options, arg) = parser.parse_args()
-    if (options.zname == None) | (options.dname == None):
+    if (options.zname is None) | (options.dname is None):
         print parser.usage
-        exit(0)
+        exit(1)
     else:
         zname = options.zname
         dname = options.dname
@@ -29,16 +34,19 @@ def main():
     zFile = zipfile.ZipFile(zname)
     passFile = open(dname)
 
-    #executor = ThreadPoolExecutor(max_workers=2)
     for line in passFile.readlines():
         password = line.rstrip()
-	#executor.submit(extract_zip,(zFile),(password))
-	extract_zip(zFile,password)
-    #executor.shutdown()
-    tiempo_final = time() 
+        if(extract_zip(zFile, password)):
+            break
+    # p=mp.Pool(8)
+    # p.map(extract_zip, zFile, passFile.readlines())
+    # p.close()
+    # p.join()
+
+    tiempo_final = time()
     tiempo_ejecucion = tiempo_final - tiempo_inicial
-    print 'El tiempo de ejecucion fue:',tiempo_ejecucion 
+    print 'El tiempo de ejecucion fue:', tiempo_ejecucion, "segundos"
+
 
 if __name__ == '__main__':
     main()
-
