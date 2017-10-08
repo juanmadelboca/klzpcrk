102c743935463808e1b7d9a8f8a326f2ce1746c4
juanmadelboca <juanmadelboca@gmail.com>
2017-10-08 18:28:32 -0300

    refactoring variables

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -3,31 +3,36 @@ import time
 
 def create_dictionary(word_length, output_name):
     start_start = time.clock()
-
-    lista = [0 for x in xrange(word_length)]
+    # creates a list of states used to make all posible words
+    dictionary_state = [0 for x in xrange(word_length)]
     x = word_length-1
-    string = "abcdefghijklnmopqrstuvwxyz0123456789"
-    file1 = file(output_name, "w")
+    alphabet = "abcdefghijklnmopqrstuvwxyz1234567890"
+    file_handler = file(output_name, "w")
+
     while(x > -1):
         result = ""
-        if lista[x] == len(string)-1:
+        if dictionary_state[x] == len(alphabet)-1:
+            print "entre if"
             for z in xrange(word_length):
-                result += string[lista[z]]
-            lista[x] = 0
+                result += alphabet[dictionary_state[z]]
+            dictionary_state[x] = 0
             x -= 1
         elif x == word_length-1:
+            print "entre elseif"
             for z in xrange(word_length):
-                result += string[lista[z]]
-            lista[x] += 1
+                result += alphabet[dictionary_state[z]]
+            dictionary_state[x] += 1
         else:
+            print "entre else"
             for z in xrange(word_length):
-                result += string[lista[z]]
-            lista[x] += 1
+                result += alphabet[dictionary_state[z]]
+            dictionary_state[x] += 1
             if x > 0:
                 x += 1
             else:
                 x = word_length-1
-        file1.write(result+"\n")
+
+        file_handler.write(result+"\n")
 
     finish_time = time.clock()
     total_time = finish_time - start_start
